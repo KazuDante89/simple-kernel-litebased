75da0b092b1e655a8e9e9b8cb60edf16ed642c5a
KazuDante89 <35647262+KazuDante89@users.noreply.github.com>
2020-08-25 12:52:34 -0400

    Update build.sh

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,21 +1,18 @@
 #!/usr/bin/env bash
 echo "Cloning dependencies"
-git clone --depth=1 https://github.com/Yasir-siddiqui/android_kernel_xiaomi_lavender -b lite-new kernel
+git clone --depth=1 https://github.com/KazuDante89/simple-kernel-litebased -b lite-new kernel
 cd kernel
-git clone --depth=1 https://github.com/Haseo97/Clang-10.0.0 clang
-git clone --depth=1 https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 -b android-9.0.0_r39 stock
-git clone --depth=1 https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 -b android-9.0.0_r39 stock_32
-git clone --depth=1 https://github.com/Yasir-siddiqui/AnyKernel3 AnyKernel
+git clone --depth=1 https://github.com/kdrag0n/proton-clang clang
+git clone --depth=1 https://github.com/KazuDante89/AnyKernel3-EAS AnyKernel
 echo "Done"
-GCC="$(pwd)/aarch64-linux-android-"
 IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
 TANGGAL=$(date +"%F-%S")
 START=$(date +"%s")
 export CONFIG_PATH=$PWD/arch/arm64/configs/lavender-perf_defconfig
-PATH="${PWD}/clang/bin:${PWD}/stock/bin:${PWD}/stock_32/bin:${PATH}"
+PATH="${PWD}/clang/bin:$PATH"
 export ARCH=arm64
-export KBUILD_BUILD_HOST=notkernel
-export KBUILD_BUILD_USER="Yasir"
+export KBUILD_BUILD_HOST=circleci
+export KBUILD_BUILD_USER="kazudante"
 # sticker plox
 function sticker() {
     curl -s -X POST "https://api.telegram.org/bot$token/sendSticker" \
@@ -28,7 +25,7 @@ function sendinfo() {
         -d chat_id="$chat_id" \
         -d "disable_web_page_preview=true" \
         -d "parse_mode=html" \
-        -d text="<b>• NotKernel •</b>%0ABuild started on <code>Circle CI/CD</code>%0AFor device <b>Xiaomi Redmi note7/7s</b> (lavender)%0Abranch <code>$(git rev-parse --abbrev-ref HEAD)</code>(master)%0AUnder commit <code>$(git log --pretty=format:'"%h : %s"' -1)</code>%0AUsing compiler: <code>$(${GCC}gcc --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')</code>%0AStarted on <code>$(date)</code>%0A<b>Build Status:</b> #Test"
+        -d text="<b>• [EAS]-Simple-Kernel-litebeta3 •</b>%0ABuild started on <code>Circle CI</code>%0AFor device <b>Xiaomi Redmi Note7</b> (lavender)%0Abranch <code>$(git rev-parse --abbrev-ref HEAD)</code>(master)%0AUnder commit <code>$(git log --pretty=format:'"%h : %s"' -1)</code>%0AUsing compiler: <code>$(${GCC}gcc --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')</code>%0AStarted on <code>$(date)</code>%0A<b>Build Status:</b>#beta"
 }
 # Push kernel to channel
 function push() {
@@ -54,15 +51,17 @@ function compile() {
    make O=out ARCH=arm64 lavender-perf_defconfig
        make -j$(nproc --all) O=out \
                              ARCH=arm64 \
-			     CROSS_COMPILE=aarch64-linux-android- \
-			     CROSS_COMPILE_ARM32=arm-linux-androideabi-
-   cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
+                             CC=clang \
+			                       CROSS_COMPILE=aarch64-linux-gnu- \
+			                       CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+   mv out/arch/arm64/boot/Image.gz-dtb AnyKernel/Image.gz
+   cp out/arch/arm64/boot/dts/qcom/sdm660-mtp_f7a.dtb AnyKernel
 }
 # Zipping
 function zipping() {
     cd AnyKernel || exit 1
-    zip -r9 NotKernel-lavender-${TANGGAL}.zip *
-    cd .. 
+    zip -r9 Simple-Kernel-litebeta3-${TANGGAL}.zip *
+    cd ..
 }
 sticker
 sendinfo
